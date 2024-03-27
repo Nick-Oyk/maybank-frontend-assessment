e925c943f4dfdd584fa578482bb8c847b6a269f8
Nicholas Ooi <ooi.yukhai@setel.com>
2024-03-27 23:54:06 +0800

    feat: add google maps and search bar with autocomplete

--- app/page.js
+++ app/page.js
@@ -1,95 +1,136 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import { Autocomplete, Box, TextField, Typography } from "@mui/material";
+import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { useMemo, useState } from "react";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
 
 export default function Home() {
+  const libraries = useMemo(() => ["places"], []);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    libraries: libraries,
+  });
+
+  if (!isLoaded) {
+    return <p>Loading...</p>;
+  }
+
+  return <Map />;
+}
+
+function Map() {
+  const center = useMemo(() => ({ lat: 4.2105, lng: 101.9758 }), []);
+  const [selected, setSelected] = useState(null);
+  const [searchValue, setSearchValue] = useState("");
+  //Later use Redux toolkit and redux thunk to store value as history table
+  console.log(searchValue.description)
+
+  const handleMapClick = (event) => {
+    if (event.latLng) {
+      setSelected({
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+      });
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      <PlacesAutocomplete setSelected={setSelected} setSearchValue={setSearchValue}/>
+      <GoogleMap
+        zoom={7}
+        center={selected ? selected : center}
+        mapContainerStyle={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+        }}
+        onClick={handleMapClick}
+        onLoad={() => console.log("Map Component Loaded...")}
+        options={{
+          mapTypeControlOptions: {
+            position: google.maps.ControlPosition.BLOCK_START_INLINE_END,
+          },
+        }}
+      >
+        {selected && <Marker position={selected} />}
+      </GoogleMap>
+    </div>
   );
 }
+
+const PlacesAutocomplete = ({ setSelected, setSearchValue }) => {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleOptionClick = (value) => {
+    if (value) {
+      getGeocode({ placeId: value.place_id }).then((results) => {
+        const { lat, lng } = getLatLng(results[0]);
+        setSelected({ lat, lng });
+      });
+      setSearchValue(value);
+      clearSuggestions();
+    }
+  };
+
+  return (
+    <div style={{ position: "absolute", top: 10, left: 10, zIndex: 1 }}>
+      <Autocomplete
+        disablePortal
+        id="map-combo-box"
+        value={data.find((x) => x.description === value.key)}
+        disabled={!ready}
+        options={data}
+        getOptionLabel={(option) =>
+          typeof option === "string" ? option : option.description
+        }
+        filterOptions={(x) => x}
+        sx={{ width: 500, backgroundColor: "white" }}
+        autoComplete
+        includeInputInList
+        filterSelectedOptions
+        onChange={(event, value) => handleOptionClick(value)}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            fullWidth
+            onInput={handleChange}
+          />
+        )}
+        renderOption={(props, option) => {
+          const { key, ...restProps } = props;
+          if (status === "OK") {
+            return (
+              <li key={key} {...restProps}>
+                <Box component="span" sx={{ mr: 2 }}>
+                  <LocationOnIcon />
+                </Box>
+                <Typography variant="body2" color="text.primary">
+                  {option.description}
+                </Typography>
+              </li>
+            );
+          }
+        }}
+      />
+    </div>
+  );
+};
